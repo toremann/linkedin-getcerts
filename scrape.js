242009c36077d57ea6847d95fb15ccbe25e5d9de
toremann <50668920+toremann@users.noreply.github.com>
2022-09-01 23:18:49 +0200

    test

--- scrape.js
+++ scrape.js
@@ -1,20 +1,25 @@
-const puppeteer = require('puppeteer');
-
+const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
+  await page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
 
-  await page.goto('https://www.linkedin.com/learning/certificates/dfe121da5ff68343d40e9b64edeec6eca89e3f7b83ba458cc0befe4d21cd6f02');
-  
+  await page.goto(
+    "https://www.linkedin.com/learning/certificates/dfe121da5ff68343d40e9b64edeec6eca89e3f7b83ba458cc0befe4d21cd6f02"
+  );
+
+  new Promise((r) => setTimeout(r, 3000));
+
   const grabCourse = await page.evaluate(() => {
-    const divTag = document.querySelector(".container")
-    return divTag.innerText
-  })
+    const divTag = document.querySelectorAll(".lls-card-headline")[0].innerText;
+    return divTag;
+  });
 
-  console.log(grabCourse)
+  console.log(grabCourse);
 
   await browser.close();
 })();
