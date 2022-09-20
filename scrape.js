653e0102ccfb0d6eece578eeb76a02f9324e3ac7
toremann <50668920+toremann@users.noreply.github.com>
2022-09-20 12:04:26 +0200

    got categories
    Categories are only available when page is forced to english.
    
    Create a loop to loop over the elements to get all categories covered by the course.
    
    Update schema to handle categories as an array?

--- scrape.js
+++ scrape.js
@@ -3,25 +3,37 @@ const connectDB = require('./db/db');
 const Certs = require('./db/Schema');
 const readlineSync = require('readline-sync');
 
+
 // Check for .env file with MONGO_URI before connecting
 
 console.log('\n\nExample: https://www.linkedin.com/learning/certificates/dfc7a36fa80cfebad55937f6f33a15189e4058ere56834f48e708a40b1474319\n');
-const url = readlineSync.question('Paste url: ')
+// const url = readlineSync.question('Paste url: ')
 
-if (process.env.MONGO_URI) {
-    connectDB();
-} else {
-    console.log('Couldnt find MONGO_URI in .env');
-    process.exit(1);
-}
+// Test url for single category
+// const url = 'https://www.linkedin.com/learning/certificates/55c480bc77abb6446ce037225af55023a4ba8fdb71f3ad32ef9937d66750dde4'
+
+// Test url for multiple categories
+const url = 'https://www.linkedin.com/learning/certificates/25a52b38f76e2dfc08beb58802cf8ae058440b30f62a1171af64f66af17bec01'
+
+// if (process.env.MONGO_URI) {
+//     connectDB();
+// } else {
+//     console.log('Couldnt find MONGO_URI in .env');
+//     process.exit(1);
+// }
 
 function timeout(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: true, args: ['--lang=en-EN,en']});
+    const browser = await puppeteer.launch({ headless: false, args: ['--lang=en-EN,en']});
     const page = await browser.newPage();
+
+    // Set english chromium, we can see categories of the course with language set to english (?)
+    await page.setExtraHTTPHeaders({
+        'Accept-Language': 'en'
+    });
 
     await page.goto(url);
 
@@ -63,10 +75,10 @@ function timeout(ms) {
 
     // Loop, expect more than one category
     const grabCategory = await page.evaluate(() => {
-        const courseVideos = document.querySelector(
-            ' '
+        const courseCategory = document.querySelector(
+            '#main-content > section > div.certificate-details__info > div.certificate-details__left-rail > section.core-section-container.my-3.course-skills > div > ul > li > a'
         ).innerText;
-        return grabCategory;
+        return courseCategory;
     });
 
     const data = {
@@ -89,6 +101,6 @@ function timeout(ms) {
     //     }
     // });
 
-    await browser.close();
-    process.exit(1)
+    // await browser.close();
+    // process.exit(1)
 })();
---
+++ selectors.js
@@ -0,0 +1,3 @@
+const grabCourseSelector = '.base-search-card__info h3';
+
+module.exports = { grabCourseSelector }
