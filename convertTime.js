56b0b31d4164f0c27b3f454508778366abe9686b
toremann <50668920+toremann@users.noreply.github.com>
2022-11-12 22:22:16 +0100

    refactor for loop

--- convertTime.js
+++ convertTime.js
@@ -1,4 +1,4 @@
-const timeArr = ['30m 20s', '2h 20m 30s', '3h 30m 20s'];
+const timeArr = ['40m 20s', '2h 30s', '3h 30m'];
 
 const newArr = [];
 
@@ -8,28 +8,17 @@ timeArr.forEach((element) => {
     newArr.push(convertTime(element));
 });
 
-console.log(newArr);
-
 let sum = { houres: 0, minutes: 0, seconds: 0 };
 
+niceForLoopBro:
 for (let i = 0; i < newArr.length; i++) {
-    if (typeof newArr[i].h == "undefined") {
-      sum.houres += 0
-    } else {
-      sum.houres += parseInt(newArr[i].h);
-    }
-    
-    if (typeof newArr[i].m == "undefined") {
-      sum.minutes += 0
-    } else {
-      sum.minutes += parseInt(newArr[i].m);
-    }
-
-    if (typeof newArr[i].s == "undefined") {
-      sum.seconds += 0
-    } else {
-      sum.seconds += parseInt(newArr[i].s);
-    }
+    if (typeof newArr[i].h !== 'undefined') sum.houres += parseInt(newArr[i].h);
+    if (typeof newArr[i].m !== 'undefined') sum.minutes += parseInt(newArr[i].m);
+    if (typeof newArr[i].s !== 'undefined') sum.seconds += parseInt(newArr[i].s);
 }
 
 console.log(sum);
+console.log(sum.minutes / 60);
+console.log(Math.floor(sum.seconds / 60))
+
+console.log(sum.houres + Math.floor(sum.minutes / 60))
