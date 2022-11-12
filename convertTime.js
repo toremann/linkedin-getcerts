ee9daae190b8949455f819f75cc13ea0c2f336ec
toremann <50668920+toremann@users.noreply.github.com>
2022-11-12 22:57:40 +0100

    convert seconds to minutes/minutes to houres

--- convertTime.js
+++ convertTime.js
@@ -1,4 +1,4 @@
-const timeArr = ['40m 20s', '2h 30s', '3h 30m'];
+const timeArr = ['40m 20s', '2h 70s', '3h 30m'];
 
 const newArr = [];
 
@@ -8,17 +8,30 @@ timeArr.forEach((element) => {
     newArr.push(convertTime(element));
 });
 
-let sum = { houres: 0, minutes: 0, seconds: 0 };
+let total = { houres: 0, minutes: 0, seconds: 0 };
 
 niceForLoopBro:
 for (let i = 0; i < newArr.length; i++) {
-    if (typeof newArr[i].h !== 'undefined') sum.houres += parseInt(newArr[i].h);
-    if (typeof newArr[i].m !== 'undefined') sum.minutes += parseInt(newArr[i].m);
-    if (typeof newArr[i].s !== 'undefined') sum.seconds += parseInt(newArr[i].s);
+    if (typeof newArr[i].h !== 'undefined') total.houres += parseInt(newArr[i].h);
+    if (typeof newArr[i].m !== 'undefined') total.minutes += parseInt(newArr[i].m);
+    if (typeof newArr[i].s !== 'undefined') total.seconds += parseInt(newArr[i].s);
 }
 
-console.log(sum);
-console.log(sum.minutes / 60);
-console.log(Math.floor(sum.seconds / 60))
+console.log(total);
+console.log(total.minutes / 60);
+console.log(Math.floor(total.seconds / 60))
 
-console.log(sum.houres + Math.floor(sum.minutes / 60))
+function toHoursAndMinutes(totalMinutes) {
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+  return `${hours}h${minutes > 0 ? ` ${minutes}m` : ''}`;
+}
+
+function toMinutes(totalSeconds) {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  return `${minutes}m${seconds > 0 ? ` ${seconds}s` : ''}`;
+}
+
+console.log('minutes to houres:', toHoursAndMinutes(total.minutes)); // 1h 40m
+console.log('seconds to minutes:', toMinutes(total.seconds))
